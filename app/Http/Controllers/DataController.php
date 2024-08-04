c52c528687943e9b3cd99ad2991e4b0945919b7a
Kurniawan Eko Putra <k.ekoptr@gmail.com>
2024-08-04 11:51:17 +0700

    test api

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 class DataController extends Controller
 {
     public function index(Request $request) {
-        
+        return response()->json([
+            'message' => 'Data received successfully',
+            'data' => $request,
+        ]);
     }
 }
